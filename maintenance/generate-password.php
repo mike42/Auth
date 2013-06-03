d791b24fd381dc8efdf98cc9bf8aefcc19e18a58
Michael Billington <michael.billington@gmail.com>
2013-06-03 15:36:11 +1000

    front-end to password generator

---
+++ maintenance/generate-password.php
@@ -0,0 +1,18 @@
+#!/usr/bin/env php
+<?php 
+require_once(dirname(__FILE__) . "/../lib/Auth.php");
+Auth::loadClass("PasswordGen");
+
+/* Generate the given number of passwords, used for batch work */
+
+if(count($argv) < 2) {
+	die("Usage: " . $argv[0] . " number");
+}
+
+$count = (int)$argv[1];
+
+for($i = 0; $i < $count; $i++) {
+	echo PasswordGen::generate() . "\n";
+}
+
+?>
