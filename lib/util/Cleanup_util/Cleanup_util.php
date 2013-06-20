53ac8b4c9f105a21ff67478eba1c1e545d8e2137
Michael Billington <michael.billington@gmail.com>
2013-06-20 10:58:53 +1000

    skeleton utility for managing more things via the web interface

---
+++ lib/util/Cleanup_util/Cleanup_util.php
@@ -0,0 +1,27 @@
+<?php
+require_once(dirname(__FILE__) . "/../util.php");
+
+class Cleanup_util extends util {
+	private static $config;
+
+	/**
+	 * Initialise utility
+	 */
+	function init() {
+		self::$util_name = "Cleanup";
+		self::verifyEnabled();
+
+		Auth::loadClass("Ou_api");
+	}
+
+	/**
+	 * Load data for web interface
+	 */
+	function admin() {
+		$data = array("current" => "Utility", "util" => self::$util_name, "template" => "main");
+		$data['service-enabled'] = Service_model::list_by_service_enabled('1');
+		$data['service-disabled'] = Service_model::list_by_service_enabled('0');
+		return $data;
+	}
+
+}
---
+++ lib/util/Cleanup_util/README.md
@@ -0,0 +1,10 @@
+Directory Cleanup Utility
+=========================
+
+This utility facilitates a few simple cleanup operations which admins might want to run from time-to-time.
+
+Installation
+------------
+Enable the utility by adding an entry to the Util list in config.php:
+
+    'Cleanup' => 'Directory Cleanup Tools'
