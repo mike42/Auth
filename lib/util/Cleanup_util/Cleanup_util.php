8d1f4f1a78adb528b3305fc690fe22e7b06ef8d4
Michael Billington <michael.billington@gmail.com>
2013-11-13 14:28:01 +1100

    Add "erase everything" and "add test data" to facilitate debugging

--- lib/util/Cleanup_util/Cleanup_util.php
+++ lib/util/Cleanup_util/Cleanup_util.php
@@ -68,6 +68,14 @@ class Cleanup_util extends util {
 						self::emptyAq();
 						$data['message'] = "The queue has been emptied.";
 						break;
+					case 'eraseLocal':
+						self::eraseLocal();
+						$data['message'] = "All local entries have been erased.";
+						break;
+					case 'dummyData':
+						self::dummyData();
+						$data['message'] = "Dummy data has been introduced.";
+						break;
 					default:
 						throw new Exception("Unknown action: $action");
 				}
@@ -186,5 +194,153 @@ class Cleanup_util extends util {
 		$service -> service_enabled = '0';
 		$service -> update();
 		return true;
+	}
+	
+	/**
+	 * Erase local data
+	 */
+	static function eraseLocal() {
+		$stack = array(Ou_model::get_by_ou_name("root"));
+		
+		while(count($stack) != 0) {
+			$ou = array_pop($stack);
+			
+			$ou -> populate_list_UserGroup();
+			foreach($ou -> list_UserGroup as $ug) {
+				$ug -> populate_list_OwnerUserGroup();
+				foreach($ug -> list_OwnerUserGroup as $oug) {
+					$oug -> delete();
+				}
+				
+				$ug -> populate_list_SubUserGroup();
+				foreach($ug -> list_SubUserGroup as $sug) {
+					$sug -> delete();
+				}
+				$ug -> delete();
+			}
+			
+			$ou -> populate_list_AccountOwner();
+			foreach($ou -> list_AccountOwner as $owner) {
+				$owner -> populate_list_Account();
+				foreach($owner -> list_Account as $account) {
+					$account -> delete();
+				}
+				
+				$owner -> populate_list_OwnerUserGroup();
+				foreach($owner -> list_OwnerUserGroup as $oug) {
+					$oug -> delete();
+				}
+				$owner -> delete();
+			}
+			
+			
+			/* Push sub-organisations to the stack */
+			$subOrgUnit = Ou_model::list_by_ou_parent_id($ou -> ou_id);
+			if(count($subOrgUnit) == 0) {
+				if($ou -> ou_name != "root") { // Avoid deleting the root unit
+					$ou -> delete();
+				}
+			} else {
+				$stack[] = $ou;
+				foreach($subOrgUnit as $l) {
+					$stack[] = $l;
+				}
+			}
+		}
+	}
+	
+	/**
+	 * Introduce dummy data
+	 */
+	static function dummyData() {
+		$root = Ou_model::get_by_ou_name("root");
+		
+		/* Pick the first domain */
+		$domain_id = "default";
+		$domainList = ListDomain_model::list_by_domain_enabled('1');
+		$domain_id = $domainList[0] -> domain_id;
+		$serviceList = ListServiceDomain_model::list_by_domain_id($domain_id);
+		$services = array();
+		foreach($serviceList as $s) {
+			if($s -> Service -> service_enabled == '1') {
+				$services[] = $s -> service_id;
+			}
+		}
+
+		/* Make orgUnits */
+		$users = Ou_api::create("people", $root -> ou_id);
+		$melbourne = Ou_api::create("melbourne", $users -> ou_id);
+		$sydney = Ou_api::create("sydney", $users -> ou_id);
+		$groups = Ou_api::create("groups", $root -> ou_id);
+		
+		/* 20 people.
+		 * Search Criteria: 2012 Top 10" from https://online.justice.vic.gov.au/bdm/popular-names */
+		$jack = AccountOwner_api::create($melbourne -> ou_id, "Example", "Jack", "jack", $domain_id, $services);
+		$olivia = AccountOwner_api::create($melbourne -> ou_id, "Example", "Olivia", "olivia", $domain_id, $services);
+		$william = AccountOwner_api::create($melbourne -> ou_id, "Example", "William", "william", $domain_id, $services);
+		$charlotte = AccountOwner_api::create($melbourne -> ou_id, "Example", "Charlotte", "charlotte", $domain_id, $services);
+		$oliver = AccountOwner_api::create($melbourne -> ou_id, "Example", "Oliver", "oliver", $domain_id, $services);
+		$ruby = AccountOwner_api::create($melbourne -> ou_id, "Example", "Ruby", "ruby", $domain_id, $services);
+		$ethan = AccountOwner_api::create($melbourne -> ou_id, "Example", "Ethan", "ethan", $domain_id, $services);
+		$chloe = AccountOwner_api::create($melbourne -> ou_id, "Example", "Chloe", "chloe", $domain_id, $services);
+		$thomas = AccountOwner_api::create($melbourne -> ou_id, "Example", "Thomas", "thomas", $domain_id, $services);
+		$mia = AccountOwner_api::create($melbourne -> ou_id, "Example", "Mia", "mia", $domain_id, $services);
+		$noah = AccountOwner_api::create($sydney -> ou_id, "Example", "Noah", "noah", $domain_id, $services);
+		$emily = AccountOwner_api::create($sydney -> ou_id, "Example", "Emily", "emily", $domain_id, $services);
+		$james = AccountOwner_api::create($sydney -> ou_id, "Example", "James", "james", $domain_id, $services);
+		$ava = AccountOwner_api::create($sydney -> ou_id, "Example", "Ava", "ava", $domain_id, $services);
+		$lucas = AccountOwner_api::create($sydney -> ou_id, "Example", "Lucas", "lucas", $domain_id, $services);
+		$amelia = AccountOwner_api::create($sydney -> ou_id, "Example", "Amelia", "amelia", $domain_id, $services);
+		$alexander = AccountOwner_api::create($sydney -> ou_id, "Example", "Alexander", "alexander", $domain_id, $services);
+		$sophie = AccountOwner_api::create($sydney -> ou_id, "Example", "Sophie", "sophie", $domain_id, $services);
+		$joshua = AccountOwner_api::create($sydney -> ou_id, "Example", "Joshua", "joshua", $domain_id, $services);
+		$zoe = AccountOwner_api::create($sydney -> ou_id, "Example", "Zoe", "zoe", $domain_id, $services);
+		
+		/* Make groups */
+		$everybody = UserGroup_api::create("global", "Employee Global", $groups -> ou_id, $domain_id);
+		$manufacturing = UserGroup_api::create("manufacturing", "Manufacturing Team", $groups -> ou_id, $domain_id);
+		$adminteam = UserGroup_api::create("adminteam", "Admin Team", $groups -> ou_id, $domain_id);
+		$marketing = UserGroup_api::create("marketing", "Marketing Team", $groups -> ou_id, $domain_id);
+		$sales = UserGroup_api::create("sales", "Sales Team", $groups -> ou_id, $domain_id);
+		$executive = UserGroup_api::create("executive", "Executive Team", $groups -> ou_id, $domain_id);
+		$logistics = UserGroup_api::create("logistics", "Logistics Team", $groups -> ou_id, $domain_id);
+		
+		/* Sub-groups */
+		UserGroup_api::addchild($everybody -> group_id, $manufacturing -> group_id);
+		UserGroup_api::addchild($everybody -> group_id, $adminteam -> group_id);
+		UserGroup_api::addchild($adminteam -> group_id, $marketing -> group_id);
+		UserGroup_api::addchild($marketing -> group_id, $sales -> group_id);
+		UserGroup_api::addchild($adminteam -> group_id, $executive -> group_id);
+		UserGroup_api::addchild($adminteam -> group_id, $logistics -> group_id);
+		
+		
+		/* Group emmbership */
+		AccountOwner_api::addtogroup($jack -> owner_id, $adminteam -> group_id);
+		AccountOwner_api::addtogroup($olivia -> owner_id, $adminteam -> group_id);
+		AccountOwner_api::addtogroup($william -> owner_id, $marketing -> group_id);
+		AccountOwner_api::addtogroup($charlotte -> owner_id, $marketing -> group_id);
+		AccountOwner_api::addtogroup($oliver -> owner_id, $sales -> group_id);
+		AccountOwner_api::addtogroup($ruby -> owner_id, $sales -> group_id);
+		AccountOwner_api::addtogroup($ethan -> owner_id, $sales -> group_id);
+		AccountOwner_api::addtogroup($chloe -> owner_id, $logistics -> group_id);
+		AccountOwner_api::addtogroup($thomas -> owner_id, $logistics -> group_id);
+		AccountOwner_api::addtogroup($mia -> owner_id, $logistics -> group_id);
+		AccountOwner_api::addtogroup($noah -> owner_id, $manufacturing -> group_id);
+		AccountOwner_api::addtogroup($emily -> owner_id, $manufacturing -> group_id);
+		AccountOwner_api::addtogroup($james -> owner_id, $manufacturing -> group_id);
+		AccountOwner_api::addtogroup($ava -> owner_id, $logistics -> group_id);
+		AccountOwner_api::addtogroup($lucas -> owner_id, $sales -> group_id);
+		AccountOwner_api::addtogroup($amelia -> owner_id, $adminteam -> group_id);
+		AccountOwner_api::addtogroup($alexander -> owner_id, $marketing -> group_id);
+		AccountOwner_api::addtogroup($sophie -> owner_id, $manufacturing -> group_id);
+		AccountOwner_api::addtogroup($chloe -> owner_id, $manufacturing -> group_id);
+		AccountOwner_api::addtogroup($joshua -> owner_id, $manufacturing -> group_id);
+		AccountOwner_api::addtogroup($zoe -> owner_id, $manufacturing -> group_id);
+		
+		/* And an executive team */
+		AccountOwner_api::addtogroup($zoe -> owner_id, $executive -> group_id);
+		AccountOwner_api::addtogroup($mia -> owner_id, $executive -> group_id);
+		AccountOwner_api::addtogroup($thomas -> owner_id, $executive -> group_id);
+		AccountOwner_api::addtogroup($james -> owner_id, $executive -> group_id);
 	}
 }
